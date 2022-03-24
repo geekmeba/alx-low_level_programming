d95a6d81a4e37d776d8839f6710497ae210be27d
geekmeba <mebratuhailu54@gmail.com>
2022-03-24 13:21:42 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,13 +3,18 @@
 int main(void)
 {
   int n;
+ 
   int a[5];
+ 
   int *p;
 
   a[2] = 1024;
+ 
   p = &n;
+ 
   /*
    * write your line of code here...
+   * main-returns void
    * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
@@ -17,7 +22,10 @@ int main(void)
    * - you are not allowed to code anything else than this line of code
    */
   ;
+ 
   /* ...so that this prints 98\n */
+ 
   printf("a[2] = %d\n", a[2]);
+ 
   return (0);
 }
